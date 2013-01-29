2ff781fc7c4b876a47366ba21e66f7b91c589980
Michael <Michael@Control>
2013-01-28 20:43:39 -0600

    Added properties and getters and setters

--- src/myconcrete/Employee.java
+++ src/myconcrete/Employee.java
@@ -1,13 +1,79 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package myconcrete;
 
 /**
  *
- * @author Michael
+ * @author Michael Grinker
  */
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private int hoursWorked;
+    private String badgeID;
+
+    public Employee(String firstName, String lastName, int hoursWorked, String badgeID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hoursWorked = hoursWorked;
+        this.badgeID = badgeID;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the hoursWorked
+     */
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    /**
+     * @param hoursWorked the hoursWorked to set
+     */
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    /**
+     * @return the badgeID
+     */
+    public String getBadgeID() {
+        return badgeID;
+    }
+
+    /**
+     * @param badgeID the badgeID to set
+     */
+    public void setBadgeID(String badgeID) {
+        this.badgeID = badgeID;
+    }
+    
+    
     
 }
