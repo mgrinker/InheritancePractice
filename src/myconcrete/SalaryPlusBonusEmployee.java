6d53cac6fc220c7d89f269c7afb9923fee5dc014
Michael <Michael@Control>
2013-01-28 20:37:07 -0600

    Start

---
+++ src/myconcrete/SalaryPlusBonusEmployee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myconcrete;
+
+/**
+ *
+ * @author Michael
+ */
+public class SalaryPlusBonusEmployee extends SalariedEmployee{
+    
+}
