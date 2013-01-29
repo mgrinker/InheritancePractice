748bfec7fb77a9a0f23a6a8b3266cdb30778e771
Michael <Michael@Control>
2013-01-28 21:04:36 -0600

    Created a startup class and added a scanner object

---
+++ src/myconcrete/StartUp.java
@@ -0,0 +1,20 @@
+
+package myconcrete;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Michael Grinker
+ */
+public class StartUp {
+    
+    public static void main(String[] args) {
+        
+        
+        //Create scanner object
+        Scanner keyboard = new Scanner(System.in);
+        
+        
+
+    }
+}
