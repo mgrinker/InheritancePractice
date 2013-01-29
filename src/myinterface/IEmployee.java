663f679b1aecbe43e0f71e800840cd772d3c47b1
Michael <Michael@Control>
2013-01-29 14:27:45 -0600

    Created Employee Interface

---
+++ src/myinterface/IEmployee.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myinterface;
+
+/**
+ *
+ * @author Michael
+ */
+public interface IEmployee {
+    String firstName();
+    String lastName();
+    String badgeID();
+    int hoursWorked();
+}
